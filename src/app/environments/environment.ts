62a5ffaee89f5d16d3b23b0f9decc91349332672
Asilbek Khujanazarov <asilbekpersonal@gmail.com>
2025-07-22 11:54:11 +0500

    Loyiha fayllari qo‘shildi2

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,5 +2,5 @@ import { Environment } from "../interfaces/environment.interface";
 
 export const environment: Environment = {
   production: false,
-  baseUrl: 'http://localhost:5233/api' // Sizning backend API URL'ingiz
+  baseUrl: 'https://yaxshijoy-back-016d95ece8ad.herokuapp.com/api' // Sizning backend API URL'ingiz
 };
