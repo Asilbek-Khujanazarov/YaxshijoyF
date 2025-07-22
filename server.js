cc900d65d0671034defa623b15cd906912410ea9
Asilbek Khujanazarov <asilbekpersonal@gmail.com>
2025-07-22 14:39:37 +0500

    Increased style budgets to fix Heroku build error

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/yn'));
+const distFolder = path.join(__dirname, 'dist/yn');
+app.use(express.static(distFolder));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/yn/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(distFolder, 'index.html'));
 });
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
