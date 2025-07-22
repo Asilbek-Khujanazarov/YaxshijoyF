e81b0ad1b316efb2bac4e0904dc53407ba9ca26c
Asilbek Khujanazarov <asilbekpersonal@gmail.com>
2025-07-22 20:02:23 +0500

    Increased style budgets to fix Heroku build error

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from "../interfaces/environment.interface";
 
 export const environment: Environment = {
   production: true,
-  baseUrl: 'https://yaxshijoy-back-016d95ece8ad.herokuapp.com/api' // Production API URL'ingiz (masalan)
+  baseUrl: 'https://yaxshi-joy.onrender.com/api' // Production API URL'ingiz (masalan)
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,5 +2,5 @@ import { Environment } from "../interfaces/environment.interface";
 
 export const environment: Environment = {
   production: false,
-  baseUrl: 'https://yaxshijoy-back-016d95ece8ad.herokuapp.com/api' // Sizning backend API URL'ingiz
+  baseUrl: 'https://yaxshi-joy.onrender.com/api' // Sizning backend API URL'ingiz
 };
