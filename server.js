2edb7198c0c338f18b090abd1bb69ab9d38a06db
Asilbek Khujanazarov <asilbekpersonal@gmail.com>
2025-07-22 14:46:57 +0500

    Increased style budgets to fix Heroku build error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const distFolder = path.join(__dirname, 'dist/yn');
+const distFolder = path.join(__dirname, 'dist/yn/browser/');
 app.use(express.static(distFolder));
 
 app.get('/*', (req, res) => {
